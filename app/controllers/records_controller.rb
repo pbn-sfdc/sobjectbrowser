1e0673210f2b16750aa77cc78ed0ab44da5c0525
pbn-lumen <bnaidu@salesforce.com>
2012-06-20 16:23:47 -0600

    add new record

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -63,21 +63,19 @@ class RecordsController < ApplicationController
 
     respond_to do |format|
       begin
-        klass = dbdc_client.materialize(name)
-        @sobjects << {:name => name, :count => klass.count}
-      if @record.save
-        format.html { redirect_to sobject_record_path(@class_name, @record), notice: 'Record was successfully created.' }
-        format.json { render json: @record, status: :created, location: @record }
-      else
-        @updateable_attributes = []
-        @record.attributes.each do |a|
-          if @klass.updateable?(a[0])
-            @updateable_attributes << a[0]
+        if @record.save
+          format.html { redirect_to sobject_record_path(@class_name, @record), notice: 'Record was successfully created.' }
+          format.json { render json: @record, status: :created, location: @record }
+        else
+          @updateable_attributes = []
+          @record.attributes.each do |a|
+            if @klass.updateable?(a[0])
+              @updateable_attributes << a[0]
+            end
           end
+          format.html { render action: "new" }
+          format.json { render json: @record.errors, status: :unprocessable_entity }
         end
-        format.html { render action: "new" }
-        format.json { render json: @record.errors, status: :unprocessable_entity }
-      end
       rescue Databasedotcom::SalesForceError => e
         @errors = []
         @errors << e
